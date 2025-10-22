eba3ea6b6922decbd778cac78951309c5061a4ac
vikasPrasad05 <vikasprasad888999@gmail.com>
2025-10-22 21:00:24 +0530

    address

--- next.config.ts
+++ next.config.ts
@@ -1,27 +1,20 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   images: {
-//     formats: ['image/avif', 'image/webp'],
-//     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-//   },
-//   compress: true,
-//   poweredByHeader: false,
-//    eslint: {
-//     ignoreDuringBuilds: true,
-//   },
-//   typescript: {
-//     // Skip type checking during build
-//     ignoreBuildErrors: true,
-//   },
-// }
-
-// module.exports = nextConfig
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // for static hosting
-  images: { unoptimized: true }, // if using images
-  assetPrefix: './', // ✅ important for GitHub Pages or custom domain
-  trailingSlash: true,
-};
+  images: {
+    formats: ['image/avif', 'image/webp'],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+  },
+  compress: true,
+  poweredByHeader: false,
+   eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // Skip type checking during build
+    ignoreBuildErrors: true,
+  },
+}
 
-export default nextConfig;
+module.exports = nextConfig
+
+
